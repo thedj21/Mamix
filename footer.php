6ad8fda66679b169c796561f9d7979d9b3698dc7
thedj21 <valentin.ovd@live.fr>
2014-06-10 18:38:25 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,14 +1,12 @@
 <div class="panel panel-default">
   <div class="panel-body">
-    <p>Licence GPL v2</p>
-  </div>
-  <div class="panel-body">
-Uptime :	
+    Uptime :	
 <?php
 $output = shell_exec('uptime');
 echo "<pre>$output</pre>";
 ?>
-</div>
+  </div>
+  <p>Licence GPL v2</p>
   <div class="panel-footer"><p>Code source : <a href="https://github.com/Creaprog/Mamix">Github</a></p></div>
 </div>
 
