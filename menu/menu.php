e6066f187d00b851dad48143b094720c6e66a1ac
My Name <my@email>
2014-06-10 17:29:27 +0200

    update

--- menu/menu.php
+++ menu/menu.php
@@ -1,8 +1,23 @@
-<nav class="navbar navbar-inverse" role="navigation">
-<ul class="nav nav-pills">
-  <li class="active"><a href="../index.php">Acceuil</a></li>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Mamix</a>
+                <li class="active"><a href="../index.php">Acceuil</a></li>
   <li><a href="../services/terminal.php">Terminal</a></li>
  <li><a href="../services/monitoring.php">Monitoring</a></li> 
  <li><a href="../services/logs.php">Logs</a></li>
-</ul>
-</nav>
+        </div>
+        <div class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right" role="form">
+            <div class="form-group">
+            </div>
+	  </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>
