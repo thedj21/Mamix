d971fd6812a3a6a8c5b0ea082906f52f9c03c148
Thedj21 <valentin.ovd@live.fr>
2014-06-10 20:23:29 +0200

    Accueil

--- menu/menu.php
+++ menu/menu.php
@@ -8,13 +8,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/index.php">Accueil</a>
+      <a class="navbar-brand" href="/index.php">Mamix</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link</a></li>
+        <li class="active"><a href="/index.php">Acceuil</a></li>
         <li><a href="#">Link</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Truc<b class="caret"></b></a>
