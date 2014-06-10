d8d916716cb87f2f3851e7fcbcd56c32d96f8538
creaprog <creaprog@gmail.com>
2014-06-10 18:18:23 +0200

    Correction mineur

--- menu/menu.php
+++ menu/menu.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
+      <a class="navbar-brand" href="index.php">Accueil</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -17,9 +17,9 @@
         <li class="active"><a href="#">Link</a></li>
         <li><a href="#">Link</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Compte<b class="caret"></b></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
+            <li><a href="#">Déconnexion</a></li>
             <li><a href="#">Another action</a></li>
             <li><a href="#">Something else here</a></li>
             <li class="divider"></li>
